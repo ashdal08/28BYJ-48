ff905bae278b8d96fd85fb244473edf98415625f
ashdal08 <69312560+ashdal08@users.noreply.github.com>
2021-05-06 13:45:16 +0200

    for my setup

--- Arduino/Code/S07_AccelStepperDemo/S07_AccelStepperDemo.ino
+++ Arduino/Code/S07_AccelStepperDemo/S07_AccelStepperDemo.ino
@@ -15,10 +15,12 @@
 
 // To help with that, I will specify my pin variables based on their color.
 
-#define blue 2
-#define pink 3
-#define yellow 4
-#define orange 5
+//ASHLEY - EDITED FOR MY NEEDS
+
+#define green 8
+#define brown 9
+#define yellow 10
+#define blue 11
 
 //Keeps track of the current direction
 //Relative to the face of the motor. 
@@ -33,13 +35,13 @@ int targetPosition = 4096;  //4096 steps per rotation when half stepping
 // Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
 // Notice, I'm passing them as Blue, Yellow, Pink, Orange (coil ends order) not
 // Blue, Pink, Yellow, Orange (firing order). 
-AccelStepper stepper1(HALFSTEP, blue, yellow, pink, orange);
+AccelStepper stepper1(HALFSTEP, green, yellow, brown, blue);
 
 void setup() {
   //Set the initial speed (read the AccelStepper docs on what "speed" means
-  stepper1.setSpeed(100.0);         
+  stepper1.setSpeed(200.0);         
   //Tell it how fast to accelerate
-  stepper1.setAcceleration(100.0); 
+  stepper1.setAcceleration(200.0); 
   //Set a maximum speed it should exceed 
   stepper1.setMaxSpeed(4000.0);      
   //Tell it to move to the target position
--- Arduino/Code/S08_AccelStepperConstantSpeed/S08_AccelStepperConstantSpeed.ino
+++ Arduino/Code/S08_AccelStepperConstantSpeed/S08_AccelStepperConstantSpeed.ino
@@ -7,6 +7,8 @@
 // Copyright (C) 2009 Mike McCauley
 // $Id: ConstantSpeed.pde,v 1.1 2011/01/05 01:51:01 mikem Exp mikem $
 
+//I HAVE FURTHER EDITED IT FOR MY NEEDS
+
 #include <AccelStepper.h>
 
 /*
@@ -15,19 +17,19 @@ Assumed Pin Mappings
 
 | ULN2003 Board | Motor Phase | Arduino Pin |
 | ------------- | ----------- | ----------- |
-| IN1           | Blue        |           2 |
-| IN2           | Pink        |           3 |
-| IN3           | Yellow      |           4 |
-| IN4           | Orange      |           5 |
+| IN1           | Green       |           8 |
+| IN2           | Brown       |           9 |
+| IN3           | Yellow      |          10 |
+| IN4           | Blue        |          11 |
 
 */
 
 
 
-//AccelStepper stepper; // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+//AccelStepper stepper; // Defaults to AccelStepper::FULL4WIRE (4 pins) on 8, 9, 10, 11
 
-//The first 4 means FULLSTEP, then the coil end pins Blue, Yellow, Pink, Orange
-AccelStepper stepper(4, 2, 4, 3, 5);  
+//The first 4 means FULLSTEP, then the coil end pins Green, Brown, Yellow, Blue
+AccelStepper stepper(4, 8, 10, 9, 11);  
 
 void setup()
 {  
@@ -35,7 +37,7 @@ void setup()
    
    //On my 28BYJ-48 with a 64:1 gear ratio my max speed was about 650
    //Positive speeds are clockwise, Negative Speeds are counter-clockwise
-   stepper.setSpeed(650);	 
+   stepper.setSpeed(100);	 
 }
 
 void loop()
